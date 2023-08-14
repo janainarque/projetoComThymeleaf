0a9a2b520fc443ed43c39be27e875e3857c55947
Janaina Arque <janainarque@gmail.com>
2023-08-14 19:51:46 -0300

    commit projeto do thymeleaf

---
+++ projetoComThymeleaf/src/main/java/br/com/senai/controller/ProdutoController.java
@@ -0,0 +1,89 @@
+package br.com.senai.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.com.senai.model.Produto;
+import br.com.senai.repository.ProdutoRepository;
+
+@Controller
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@GetMapping("/")
+	public String paginaPrincipal() {
+		return "index";
+	}
+	
+	@GetMapping("/produto")
+	public String listarProdutos(Model model) {
+		List<Produto> produtos = produtoRepository.findAll();
+		model.addAttribute("produtos", produtos);
+		return "produtos";
+	}
+	
+	@GetMapping("/cadastrarProduto")
+	public String paginaAdicionarProduto(Produto produto) {
+		return "adicionar_produto";
+	}
+	
+	@PostMapping("/adicionarProduto")
+	public String adicionaProduto(@Valid Produto produto, 
+			Errors erros, BindingResult result, Model model) {
+		if(result.hasErrors() || (null != erros && erros.getErrorCount() > 0)) {
+			return "adicionar_produto";
+		}
+		produtoRepository.save(produto);
+		return "redirect:/produto";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String paginaAtualizarProduto(
+			@PathVariable("id") long id, Model model) {
+		Produto produto = produtoRepository.findById(id)
+				.orElseThrow(() -> 
+				new IllegalArgumentException("Identificador do produto é inválido" + id));
+		
+		model.addAttribute("produto", produto);
+		return "editar_produto";
+	}
+	
+	@PostMapping("/atualizar/{id}")
+	public String atualizarProduto(@PathVariable("id") long id,
+			@Valid Produto produto, BindingResult result,
+			Model model) {
+		if(result.hasErrors()) {
+			produto.setId(id);
+			return "editar_produto";
+		}
+		produtoRepository.save(produto);
+		return "redirect:/produto";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deletarProduto(
+			@PathVariable("id") long id, Model model) {
+		Produto produto = produtoRepository.findById(id)
+				.orElseThrow(()-> 
+				new IllegalArgumentException("Identificador do produto inválido" + id));
+		produtoRepository.delete(produto);
+		return "redirect:/produto";
+	}
+	
+	
+	
+	
+
+}
---
+++ projetoComThymeleaf/src/main/java/br/com/senai/model/Produto.java
@@ -0,0 +1,89 @@
+package br.com.senai.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Produto {
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "Campo obrigatório")
+	@Size(min = 2, max = 254, message="Nome dever conter entre 2 e 254 caracteres") 
+	private String nome;
+	
+	@Min(0)
+	private int quantidade;
+	
+	@Min(0)
+	private double preco;
+	private String descricaoProduto;
+	private String urlImagem;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+
+	public double getPreco() {
+		return preco;
+	}
+
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+
+
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+
+
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+
+
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+}
